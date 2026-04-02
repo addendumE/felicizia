d5819d1b0c5d17cc78d4a82c90611444d1963b94
maurizio bertero <maurizio.bertero@urmet.com>
2026-04-02 07:37:35 +0200

    debug

--- main/esp32/UsRange.cpp
+++ main/esp32/UsRange.cpp
@@ -4,12 +4,13 @@
 
 #define BUF_SIZE 128
 
-static char *TAG="UsRange";
+static const char *TAG="UsRange";
 UsRange::UsRange(int com_port,int rxPin):
     Thread("UsRange"),
     com_port((uart_port_t)com_port),
     distance(0.0f)
 {
+    ESP_LOGI(TAG,"init on com %d gpio %d",com_port,rxPin);
     bzero((void*)&uart_config,sizeof(uart_config));
     uart_config.baud_rate = 9600;
     uart_config.data_bits = UART_DATA_8_BITS;
